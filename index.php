61b94e7ef8d3953ebe2e83ff061ff7f199eacc3f
ramon <ramonduarte1@hotmail.com>
2018-08-18 10:42:45 -0300

    removi php

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        //alterado netbeans de novo
-        echo 'oi';
+
         
         ?>
     </body>
