97fb81a99a792b518affd4390549c6a903a43ddf
usuario <usuario@estagio1.supra>
2018-08-20 17:35:46 -0300

    Mostrar disciplinas e aluno em matricular

--- Consultas/consultaAlunosMatriculados.php
+++ Consultas/consultaAlunosMatriculados.php
@@ -15,25 +15,8 @@ foreach ($_SESSION['alunos_matriculados'] as $matricula => $aluno) {
     $pesquisa = $_POST['pesq_aluno'];
 
     $pattern = '/' . $pesquisa . '/'; //Padrão a ser encontrado na string $tags
-    if (preg_match($pattern, $aluno['nome'])) {
-        echo "<table>
-                   <tr>
-                    <td>
-                     <form action='../controller/LancarNotaController.php' method='post'>
-                            <input size='4' readonly name='matricula' value='" . $matricula . "'>
-                            <input name='nome' type='text' value='" . $aluno['aluno']['nome'] . "'>
-                            <input name='email' type='text' value='" . $aluno['aluno']['email'] . "'>
-                            <input name='endereco' type='text' value='" . $aluno['aluno']['endereco'] . "'>
-                            <input name='telefone' type='text' value='" . $aluno['aluno']['telefone'] . "'>
-                            <button>salvar</button>
-                     </form>
-                    </td>
-                    <td>
-                    <form action='../controller/AlunoController.php' method='post'>
-                       <input name='matricula' type='hidden' value='" . $matricula . "'>
-                       <input name='apagar' type='hidden' value='apagar'>
-                       <button>apagar</button></td>
-                    </form>
-                    </tr></table><br>";
+    if (preg_match($pattern, $aluno['aluno']['nome'])) {
+        echo $aluno[''];
     }
+    
 }
--- view/lancar_notas.php
+++ view/lancar_notas.php
@@ -19,7 +19,6 @@ and open the template in the editor.
                     data: 'pesq_aluno=' + $('#pesq_aluno').val(),
                     success: function (data) {
                         $('#tabelaAluno').html(data);
-
                     }
                 });
             }
@@ -45,52 +44,6 @@ and open the template in the editor.
         <br><br>
         <div class="centralizado" id="tabelaAluno"></div>
         <br><br>
-        <table border='1'>
-
-            <?php if (isset($_SESSION['alunos_matriculados'])): ?>
-                <tr><th colspan="11" class="centralizado">Aluno</th></tr>
-                <tr>
-                    <td>Matricula</td>
-                    <td><input size="4" readonly value="<?php echo $_SESSION['alunos_matriculados'][1]; ?>"></td>
-                    <td>Aluno</td>
-                    <td  colspan="3"><input readonly value="<?php echo $_SESSION['alunos'][$_SESSION['aluno_disciplina']['aluno']]['nome']; ?>"></td>
-                    <td>Email</td>
-                    <td><input readonly value="<?php echo $_SESSION['alunos'][$_SESSION['aluno_disciplina']['aluno']]['email']; ?>"></td>
-                </tr>
-                <tr><th colspan="7" class="centralizado">Disciplinas</th></tr>
-                <?php foreach ($_SESSION['alunos_matriculados'] as $codigo): ?>
-                    <tr>
-                        <td>Codigo</td>
-                        <td><input size="4" name="" readonly value="<?php echo $codigo; ?>"></td>
-                        <td>Disciplina</td>
-                        <td><input readonly name="" value="<?php echo $_SESSION['disciplinas'][$codigo]['nome'] ?>"></td>
-                        <td>Notas 1</td>
-                        <td><input type="text" size="2"></td>
-                        <td>Notas 2</td>
-                        <td><input type="text" size="2"></td>
-                        <td>Notas 3</td>
-                        <td><input type="text" size="2"></td>
-                        <td><button>salvar</button></td>
-                    </tr>
-                <?php endforeach; ?>
-                <tr><th colspan="7" class="centralizado">Turmas</th></tr>
-                        <?php foreach ($_SESSION['aluno_disciplina']['turmas'] as $codigo): ?>
-                    <tr>
-                        <td>Codigo</td>
-                        <td><input size="4" name="" readonly value="<?php echo $codigo; ?>"></td>
-                        <td>Turmas</td>
-                        <td colspan="3"><input size="52" readonly name="" value="<?php echo $_SESSION['turmas'][$codigo]['nome'] ?>"></td>
-                    </tr>
-                <?php endforeach; ?>
-                <tr>
-                    <td colspan="6"></td>
-                    <td>
-                        <form action="" method="post">
-                            <!--pega a sessao aluno_disciplina e faz a matricula-->
-                            <button>Salvar</button></td>
-                    </form>
-                </tr>
-            <?php endif; ?>
-        </table> 
+        
     </body>
 </html>
